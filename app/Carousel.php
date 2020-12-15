d982ae23e2e790606a81171989297eacf2a050a2
zhang <zhang@example.com>
2020-12-15 03:46:41 +0000

    20201215

--- app/Carousel.php
+++ app/Carousel.php
@@ -9,11 +9,12 @@ class Carousel extends Model
     protected $fillable = ['image'];
 
 
-    // public static function boot()
-    // {
-    //     parent::boot();
-    //     static::saving(function($model){
-    //         $model->image = '/'.$model->image;
-    //     });
-    // }
+    public static function boot()
+    {
+        parent::boot();
+        static::saving(function($model){
+            $model->image = '/uploads/'.$model->image;
+        });
+    }
+
 }
