c3002d64496e2eefee5d8c63bb2a5878833f934b
BrothelUndercover <summer@gmail.com>
2021-01-16 03:56:04 +0000

    20210116

--- app/Http/Controllers/TopicesController.php
+++ app/Http/Controllers/TopicesController.php
@@ -84,7 +84,7 @@ class TopicesController extends Controller
     public function search(Request $request,City $city)
     {
         $keyword = $request->input('query');
-        $topices = Topice::search($keyword)->with('user','proviArea','cityArea','countyArea','category')->where('is_check',true)->orderBy('created_at','desc')->paginate(12);
+        $topices = Topice::search($keyword)->with('user','proviArea','cityArea','countyArea','category')->orderBy('created_at','desc')->paginate(12);
         $hotCities = $city->getHotCitys();
         return view('topices.search',compact('topices','hotCities'));
     }
